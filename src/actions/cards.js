0ef4da4502d199d66b6125fda2affa8dbaf5f83c
pl12133 <krirken@gmail.com>
2015-11-25 02:06:18 -0500

    Added undoMove and redoMove actions

--- src/actions/cards.js
+++ src/actions/cards.js
@@ -1,6 +1,8 @@
 export const MOVE_CARD = 'MOVE_CARD';
 export const SHUFFLE_CARDS = 'SHUFFLE_CARDS';
 export const FLIP_CARD = 'FLIP_CARD';
+export const UNDO_MOVE = 'UNDO_MOVE';
+export const REDO_MOVE = 'REDO_MOVE';
 
 export function shuffleCards() {
   return {
@@ -18,5 +20,15 @@ export function flipCard(card) {
   return {
     type: FLIP_CARD,
     card
+  }
+}
+export function undoMove() {
+  return {
+    type: UNDO_MOVE
+  }
+}
+export function redoMove() {
+  return {
+    type: REDO_MOVE 
   }
 }
