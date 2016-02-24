1d6a6af1176690e9b4d2f25761f3d39e8ab69806
pl12133 <krirken@gmail.com>
2016-02-24 10:47:57 -0500

    Cleanup

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,12 +4,10 @@ import React, { Component, PropTypes } from 'react';
 
 /* Styles */
 import cssStyles from './styles/';
-let cardStyle = cssStyles.styles || '';
-let unflippedStyle = cssStyles.unflipped || '';
-let flippedStyle = cssStyles.flipped || '';
+const cardStyle = cssStyles.styles || '';
+const unflippedStyle = cssStyles.unflipped || '';
+const flippedStyle = cssStyles.flipped || '';
 //
-
-// const DISPLAY_NAME = '<Card>';
 const propTypes = {
   name: PropTypes.string.isRequired,
   offsetLeft: PropTypes.number,
@@ -24,17 +22,6 @@ const propTypes = {
   onDoubleClick: PropTypes.func
 };
 
-// const Card = ({name,
-//               offsetLeft,
-//               offsetTop,
-//               offsetWidth,
-//               offsetHeight,
-//               flipped,
-//               handleBeginDragDrop,
-//               onMouseDown,
-//               onTouchStart,
-//               onTouchTap,
-//               onDoubleClick}) => {
 class Card extends Component {
   constructor (props) {
     super(props);
@@ -52,31 +39,36 @@ class Card extends Component {
     });
   }
   render () {
-    let { name,
-          flipped,
-          offsetLeft = 0,
-          offsetTop = 0,
-          offsetWidth = 0,
-          offsetHeight = 0 } = this.props;
+    let {
+      name,
+      flipped,
+      offsetLeft = 0,
+      offsetTop = 0,
+      offsetWidth = 0,
+      offsetHeight = 0
+    } = this.props;
     let className = cardStyle + ' ';
     className += (flipped)
       ? flippedStyle
       : unflippedStyle;
 
     return (
-      <div id={name} className={className}
-                     style={ {
-                       zIndex: 10,
-                       position: 'absolute',
-                       width: offsetWidth + 'px',
-                       height: offsetHeight + 'px',
-                       top: offsetTop + 'px',
-                       left: offsetLeft + 'px'} }
-                     onMouseDown={this.props.onMouseDown || ((e) => this.props.handleBeginDragDrop(e, this))}
-                     onTouchStart={this.props.onTouchStart || ((e) => this.props.handleBeginDragDrop(e.changedTouches[0], this))}
-                     onTouchTap={this.props.onTouchTap}
-                     onDoubleClick={this.props.onDoubleClick} >
-      </div>
+      <div
+        id={name}
+        className={className}
+        style={ {
+          zIndex: 10,
+          position: 'absolute',
+          width: offsetWidth + 'px',
+          height: offsetHeight + 'px',
+          top: offsetTop + 'px',
+          left: offsetLeft + 'px'
+        } }
+        onMouseDown={this.props.onMouseDown || ((e) => this.props.handleBeginDragDrop(e, this))}
+        onTouchStart={this.props.onTouchStart || ((e) => this.props.handleBeginDragDrop(e.changedTouches[0], this))}
+        onTouchTap={this.props.onTouchTap}
+        onDoubleClick={this.props.onDoubleClick}
+      />
     );
   }
 }
