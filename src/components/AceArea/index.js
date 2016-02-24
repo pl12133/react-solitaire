1978a8ff78de14229a4ab38d910f54274bb4b8d0
pl12133 <krirken@gmail.com>
2016-02-24 10:47:22 -0500

    Change to function component

--- src/components/AceArea/index.js
+++ src/components/AceArea/index.js
@@ -3,22 +3,10 @@ import React, {Component, PropTypes } from 'react';
 /* eslint-enable no-unused-vars*/
 import styles from './styles';
 
-class AceArea extends Component {
-  constructor (props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <div id={'aceArea'} className={styles}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-AceArea.propTypes = {
-
-};
+const AceArea = ({children}) => (
+  <div id={'aceArea'} className={styles}>
+    {children}
+  </div>
+);
 
 export default AceArea;
