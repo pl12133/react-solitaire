3f6537a936e98844facb57461e1286cca0ae300f
pl12133 <krirken@gmail.com>
2016-03-17 20:38:41 -0400

    Move all card zones into one file

---
+++ src/constants/cardZones.js
@@ -0,0 +1,21 @@
+export default {
+  LOWER_STACKS: [
+    'STACK-1',
+    'STACK-2',
+    'STACK-3',
+    'STACK-4',
+    'STACK-5',
+    'STACK-6',
+    'STACK-7'
+  ],
+  ACE_STACKS: [
+    'ACE-1',
+    'ACE-2',
+    'ACE-3',
+    'ACE-4'
+  ],
+  dealArea: {
+    faceDown: 'DEAL-AREA-FACEDOWN',
+    faceUp: 'DEAL-AREA-FACEUP'
+  }
+};
